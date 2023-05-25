ad8e9653085f103c2bd23e29e2a5e4c678131cf3
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-24 21:17:16 -0400

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+function grocery(name) {
+    console.log(name);
+}
+
+grocery("Hi I'm Lois Griffin, I'm married to my husband; Peter Griffin. We have 3 kids; Meg, Chris, & Stewie. I also have a talking dog named Brian.")
